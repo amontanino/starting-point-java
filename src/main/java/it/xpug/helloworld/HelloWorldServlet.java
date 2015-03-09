6b4c2e973cdc181aa55392f79bcf5a1f77736be8
Alberto <alberto.digioacchino01@universitadipavia.it>
2015-03-09 16:45:23 +0100

    corsi

--- src/main/java/it/xpug/helloworld/HelloWorldServlet.java
+++ src/main/java/it/xpug/helloworld/HelloWorldServlet.java
@@ -24,9 +24,9 @@ public class HelloWorldServlet extends HttpServlet {
 
 		response.setContentType("application/json");
 		PrintWriter writer = response.getWriter();
-		String json = "[{\"title\": \"test corso\", \"description\": \"corso che parla di JAVA\",\"teacher\": \"docente\", \"price\": \"120euro\", \"date\":\"23/11/2015\"}" +
-				"{\"title\": \"test corso 2\", \"description\": \"corso che parla di C#\",\"teacher\": \"docente2\", \"price\": \"140euro\", \"date\":\"28/11/2015\"}" +
-				"{\"title\": \"test corso 3\", \"description\": \"corso che parla di Ruby\",\"teacher\": \"docente3\", \"price\": \"540euro\", \"date\":\"28/12/2015\"}" +
+		String json = "[{\"title\": \"test corso\", \"description\": \"corso che parla di JAVA\",\"teacher\": \"docente\", \"price\": \"120euro\", \"date\":\"23/11/2015\"}," +
+				"{\"title\": \"test corso 2\", \"description\": \"corso che parla di C#\",\"teacher\": \"docente2\", \"price\": \"140euro\", \"date\":\"28/11/2015\"}," +
+				"{\"title\": \"test corso 3\", \"description\": \"corso che parla di Ruby\",\"teacher\": \"docente3\", \"price\": \"540euro\", \"date\":\"28/12/2015\"}," +
 				"{\"title\": \"test corso xp\", \"description\": \"corso che parla di XP\",\"teacher\": \"docente4\", \"price\": \"640euro\", \"date\":\"1/11/2015\"}" +
 				"]";
 		writer.write(format(json, seven()));
