d8769a903450df321ed848b738aafecfdfabef85
amontanino <a.montanino@vesenda.com>
2015-03-10 13:34:54 +0100

    edit the style

--- src/main/webapp/javascripts/courses.js
+++ src/main/webapp/javascripts/courses.js
@@ -1,13 +1,6 @@
 angular.module('coursesApp', [])
     .controller('coursesController', ['$scope', '$rootScope', '$http', function ($scope, $rootScope, $http) {
 
-        //$scope.callService = function ()
-        //{
-
-        //"{\"title\": \"test corso\", \"description\": \"corso che parla di JAVA\",\"teacher\": \"docente\", \"price\": \"120euro\", \"date\":\"23/11/2015\"}";
-        // Call web service
-
-
         function elaborateResult(result) {
             $scope.listOfCourses = result;
         }
@@ -15,18 +8,5 @@ angular.module('coursesApp', [])
        $http.get('/helloworld').success(function(data){
        elaborateResult(data);
        })
-
-        /*var result = [{
-            title: 'test corso',
-            description: 'corso che parla di JAVA',
-            teacher: 'docente',
-            price: '10,00 euro',
-            date: '23/11/2015',
-        }];*/
-
-        //elaborateResult(result);
-        //}
-
-
 
     }]);
