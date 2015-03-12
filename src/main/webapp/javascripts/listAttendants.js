dbf5d12125ae18cbc65c260b15b2d6a40cc318ad
Alberto Bandini <alberto.bandini.93@gmail.com>
2015-03-12 17:41:48 +0100

    num tot

--- src/main/webapp/javascripts/listAttendants.js
+++ src/main/webapp/javascripts/listAttendants.js
@@ -5,9 +5,10 @@ $http.get('/api/attendants').success(function(data){
 	var coursesAttendants = {};
 	for (var i = 0; i < data.length; i ++){
 		if (coursesAttendants[data[i].course_id] === undefined) { 
-			coursesAttendants[data[i].course_id] = [data[i]]; 
+			coursesAttendants[data[i].course_id] = {count: data[i].num_attendants,data: [data[i]]}; 
 		} else {
-			coursesAttendants[data[i].course_id].push(data[i]); 
+			coursesAttendants[data[i].course_id].data.push(data[i]); 
+			coursesAttendants[data[i].course_id].count += data[i].num_attendants;
 		}
 	}
 	$scope.listOfAttendants = [coursesAttendants];
